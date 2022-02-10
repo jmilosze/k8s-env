f7c524861cda0d0ee78d4488bb1f5c5ceda2012f
jacek <jacek.miloszewski@gmail.com>
2022-02-10 22:58:25 +0000

    added kong

--- main.tf
+++ main.tf
@@ -45,10 +45,46 @@ resource "kubernetes_secret" "cert_manager_ca" {
   }
 }
 
+resource "time_sleep" "wait_after_cert_manager" {
+  depends_on = [helm_release.cert_manager]
+  create_duration = "30s"
+}
 
 resource "helm_release" "ca_issuer" {
-  depends_on = [kubernetes_secret.cert_manager_ca]
+  depends_on = [time_sleep.wait_after_cert_manager]
 
-  name       = "cluster-issuer"
-  chart      = "./charts/cluster-issuer"
+  name  = "cluster-issuer"
+  chart = "./charts/cluster-issuer"
+}
+
+resource "kubernetes_namespace" "kong" {
+  metadata {
+    name = "kong"
+  }
+}
+
+resource "helm_release" "kong" {
+  depends_on = [kubernetes_namespace.kong]
+
+  name       = "kong"
+  repository = "https://charts.konghq.com"
+  chart      = "kong"
+  namespace  = "kong"
+
+  set {
+    name  = "installCRDs"
+    value = "false"
+  }
+  set {
+    name  = "proxy.type"
+    value = "NodePort"
+  }
+  set {
+    name  = "http.nodePort"
+    value = 32080
+  }
+  set {
+    name  = "tls.nodePort"
+    value = 32443
+  }
 }
